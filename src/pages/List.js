a3e716a53158daa6d6338ec6da7d33fecc353b16
rogeriodeoliveira <blogtecnologiacominformacao@gmail.com>
2019-10-03 15:30:25 -0300

    Fim Desenvolvendo o app mobile

---
+++ src/pages/List.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+export default function List() {
+
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storeTechs => {
+            const techsArray = storeTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image source={logo} style={styles.logo}/>
+            <ScrollView>
+                {techs.map(tech =>  <SpotList key={tech} tech={techs} /> )}
+            </ScrollView>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 20,
+    },
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 10,
+    },
+});
